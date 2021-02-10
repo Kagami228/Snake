ae7c9e8a351cc75e6ebaacd30cc4b41bf04152a1
Sasha <sasha.dovgan@gmail.com>
2021-02-10 19:56:28 +0200

    second

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1, y1 = 3;
+            char simbol = '*';
+
+            Drowi(x1, y1, simbol);
+            int x2 = 5, y2 = 5;
+            char sim = '&';
+            Drowi(x2, y2, sim);
             Console.ReadLine();
+        }
+        static void Drowi(int x,int y, char s)
+        {
+            Console.SetCursorPosition(x, y);
+
+            Console.Write(s);
         }
     }
 }
