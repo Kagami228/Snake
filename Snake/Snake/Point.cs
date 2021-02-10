64bc7635c174c59743ab85407f18b991117b35c7
Sasha <sasha.dovgan@gmail.com>
2021-02-10 20:38:59 +0200

    incups

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -9,6 +9,10 @@ namespace Snake
         public int x, y;
         public char s;
         
+        public Point(){}
+        public Point(int xx,int yy, char ss) {
+            x = xx;y = yy;  s = ss;
+        }
         public void Drowi()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1; p1.y = 3; p1.s = '*';
+            Point p1 = new Point(1,1,'s');
             p1.Drowi();
             Point p2 = new Point();
             p2.x = 4;p2.y = 5;p2.s = '$';
